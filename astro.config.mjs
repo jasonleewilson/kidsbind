486e1fac2cd3a4b9f9dfb94fa9b3642dc50e4d59
Jason Lee Wilson <robotici@yahoo.com>
2025-04-12 20:15:48 -0500

    fp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+
+  integrations: [react()],
 });
