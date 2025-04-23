22689aeb293beea6aa0f1e6778503cbbbacda4c3
Jason Lee Wilson <robotici@yahoo.com>
2025-04-22 23:57:22 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -10,16 +9,17 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), basicSsl()],
-    server: {
-      https: true,
-    },
+    plugins: [tailwindcss()],
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
       // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
       alias: import.meta.env.PROD && {
         "react-dom/server": "react-dom/server.edge",
       },
+    },
+    build: {
+      minify: true,
+      cssMinify: true,
     },
   },
 
--- src/components/Blank_Comp.tsx
+++ src/components/Blank_Comp.tsx
@@ -1,10 +1,18 @@
-import LocalWeather from "./LocalWeather";
+// import LocalWeather from "./LocalWeather";
 
 export default function Blank() {
   return (
     <div className='grow p-8 bg-amber-500'>
       <h1>Content</h1>
-      <LocalWeather client:load />
+      {/* <LocalWeather client:load /> */}
+      <div className='bg-[#f81f1f] rounded-md inline-flex justify-center items-center gap-2.5'>
+        <a
+          href='/about'
+          className="justify-start p-2.5 text-white text-xs font-normal font-['Inter'] hover:bg-amber-700"
+        >
+          VIEW NOW
+        </a>
+      </div>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, nisi
         ipsa est deleniti aut quibusdam sapiente sunt quaerat quas eos
