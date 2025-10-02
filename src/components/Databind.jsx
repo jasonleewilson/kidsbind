f7532ca1153d774eca57caba236aecd073aa467d
Jason Wilson <wilson@Mac.home>
2025-10-02 01:29:03 -0500

    updated layout

--- src/components/Databind.jsx
+++ src/components/Databind.jsx
@@ -8,32 +8,39 @@ export function Databind(props) {
   };
 
   return (
-    <div className='grow content-center'>
-      <div className='h-1/2 upper py-16'>
-        <p className='uppercase text-7xl text-red-500 wrap-break-word text-wrap pb-4 text-center overflow-auto'>
+    <div className='content-center'>
+      <div class="grid md:grid-cols-2 gap-4">
+        {/* <!-- The first child div will occupy the first row --> */}
+        <div class="text-white p-4 rounded">
+          <div class="container mx-auto text-white p-4 rounded">
+            <div class="flex items-center justify-center">
+              <form onReset={() => setInputValue("")}>
+                <label htmlFor='enter-text' className='sr-only'></label>
+                <input
+                  className='mb-4 p-4 text-center text-lg md:text-2xl text-black border-1 outline-black focus:border-black focus:outline focus:outline-black overflow-hidden'
+                  type='text'
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  placeholder='Enter ABCs or 123s here'
+                // maxLength='26'
+                // autofocus
+                />
+                <br />
+                <input
+                  type='reset'
+                  value='Reset'
+                  className='p-2 px-4 w-full mx-auto bg-gray-300 text-black border border-black rounded hover:bg-gray-400 cursor-pointer'
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- The second child div will occupy the second row --> */}
+
+        <p className='text-output'>
           {inputValue}
         </p>
-      </div>
-
-      <div className='h-1/2 w-fit mx-auto lower'>
-        <form onReset={() => setInputValue("")}>
-          <label htmlFor='enter-text' className='sr-only'></label>
-          <input
-            className='mx-auto text-center m-4 p-4 text-lg md:text-2xl text-black border-1 outline-black focus:border-black focus:outline focus:outline-black overflow-hidden'
-            type='text'
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder='Enter ABCs or 123s here'
-            // maxLength='26'
-            // autofocus
-          />
-          <br />
-          <input
-            type='reset'
-            value='Reset'
-            className='p-2 px-4 w-full mx-auto bg-gray-300 text-black border border-black rounded hover:bg-gray-400 cursor-pointer'
-          />
-        </form>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,21 +20,7 @@ const Navbar: React.FC = () => {
                 href='/'
                 className='flex items-center py-5 px-2 text-gray-700 hover:text-gray-900'
               >
-                {/* <svg
-                  className='h-6 w-6 mr-1 text-blue-400'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth={2}
-                    d='M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z'
-                  />
-                </svg> */}
-                <span className='font-bold'>KidsBind</span>
+                <span className='bold'>Kids</span><span className='font-extralight'>Bind</span>
               </a>
             </div>
 
@@ -102,15 +88,9 @@ const Navbar: React.FC = () => {
 
       {/* Mobile Menu */}
       <div className={`${isMobileMenuOpen ? "block" : "hidden"} md:hidden`}>
-        <a href='/about' className='block py-2 px-4 text-sm hover:bg-gray-200'>
+        <a href='/about' className='block text-right py-2 px-4 text-sm hover:bg-gray-200'>
           About
         </a>
-        {/* <a href='/blank' className='block py-2 px-4 text-sm hover:bg-gray-200'>
-          Blank
-        </a>
-        <a href='/audio' className='block py-2 px-4 text-sm hover:bg-gray-200'>
-          Audio
-        </a> */}
       </div>
     </nav>
   );
